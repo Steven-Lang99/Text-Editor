c09b2e2ef19b97096229f855539f6400503fc0ac
Steve Lang <langloiss0003@my.uwstout.edu>
2022-08-21 18:35:31 -0500

    added plugins

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,21 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      })
+
+
     ],
 
     module: {
       rules: [
-        
+
       ],
     },
   };
